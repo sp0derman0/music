ca1d9bcad4172cc71ea6d56e3b2279d013fac97d
sp0derman0 <leonbase9@gmail.com>
2025-02-08 12:43:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://<db_username>:<db_password>@cluster0.k33zh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  mongodbUri : "mongodb+srv://sp0der:<db_password>@cluster0.k33zh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
